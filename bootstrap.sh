2ab01ee36b2068c1def6d7a208812e3d66101a1b
Thomas Willert <thomas@willert.dk>
2019-10-03 22:21:07 +0200

    adding files

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,21 +2,21 @@
 
 id
 yum install -y epel-release
-yum-config-manager --enable epel-testing 
-yum install -y git ansible python2-boto python2-boto3 python2-botocore
+#yum-config-manager --enable epel-testing 
+#yum install -y git ansible python2-boto python2-boto3 python2-botocore
 #
 # Botocore bug workaround
 # current CentOS, python boto packages are buggy.
 #
-yum install -y python2-pip
-pip install --upgrade pip
-pip install --upgrade botocore boto3
+#yum install -y python2-pip
+#pip install --upgrade pip
+#pip install --upgrade botocore boto3
 # END Botocore bug workaround
 
-curl -o /etc/ansible/ec2.py https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py
-chmod +x /etc/ansible/ec2.py
-curl -o /etc/ansible/ec2.ini https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.ini
-sed -i -e 's/^regions = all/regions = auto/' /etc/ansible/ec2.ini
+#curl -o /etc/ansible/ec2.py https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py
+#chmod +x /etc/ansible/ec2.py
+#curl -o /etc/ansible/ec2.ini https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.ini
+#sed -i -e 's/^regions = all/regions = auto/' /etc/ansible/ec2.ini
 
 test -d .ssh || mkdir -m 0700 .ssh
 cp /vagrant/config /vagrant/thomas* .ssh/
@@ -26,8 +26,8 @@ chmod 0600 .ssh/thomas*
 cat > ~vagrant/bootstrap-git.sh <<EOF
 mkdir git
 cd git
-git clone git@github.com:twillert/kubernetes-sandbox.git
-cd kubernetes-sandbox
+git clone git@github.com:twillert/ceph-sandbox.git
+cd ceph-sandbox
 git config --global user.name "Thomas Willert"
 git config --global user.email thomas@willert.dk
 git config --global push.default simple
--- dot.bashrc
+++ dot.bashrc
@@ -2,7 +2,7 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 
 export AWS_REGION=eu-central-1
 export AWS_ACCESS_KEY_ID=AKIARQIXZBFY2FZIT67E
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_SECRET_ACCESS_KEY=***************************************
 
 export ANSIBLE_PRIVATE_KEY_FILE=~/.ssh/thomas_internet_home
 export ANSIBLE_REMOTE_USER=centos
